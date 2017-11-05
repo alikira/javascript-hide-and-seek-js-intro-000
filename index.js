d507256f422d9ec8613ed9c4c6a7501530b4d8ea
Ali Karchoud <karchoud.ali@gmail.com>
2017-11-05 09:50:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function deepestChild(){
   return document.querySelector('#grand-node div div div div');
 
 }
+
+
+function increaseRankBy(n){
+    var rankedLists=document.querySelectorAll(".ranked-list");
+    for(let i=0;i<rankedLists.length;i++){
+      var list=rankedLists[i];
+      list.children.foreach(function(value){
+        value.innerHTML(parseInt(value.innerHTML())+n);
+      });
+    }
+}
